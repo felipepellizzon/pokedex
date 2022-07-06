b82b34e515ede7c3c7ae0ca69b7085c728260cc5
felipepellizzon <fapellizzon@gmail.com>
2022-07-05 22:42:21 -0300

    add: font and css in title / shiny generation

--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,20 @@ document.addEventListener("DOMContentLoaded", () =>{
     let spanKanto = document.getElementById("first")
     spanKanto.addEventListener('click', renderFirstGeneration)
 
+    let spanKantoShiny = document.getElementById("firstShiny")
+    spanKantoShiny.addEventListener('click', renderFirstGenerationShiny)
+
     let spanJohto = document.getElementById("second")
     spanJohto.addEventListener('click', renderSecondGeneration)
 
+    let spanJohtoShiny = document.getElementById("secondShiny")
+    spanJohtoShiny.addEventListener('click', renderSecondGenerationShiny)
+
     let spanHoenn = document.getElementById("third")
     spanHoenn.addEventListener('click', renderThirdGeneration)
+
+    let spanHoennShiny = document.getElementById("thirdShiny")
+    spanHoennShiny.addEventListener('click', renderThirdGenerationShiny)
 
     let deletar = document.getElementById("deletar")
     deletar.addEventListener('click', removerConteudo)
@@ -24,6 +33,13 @@ async function renderFirstGeneration(){
     await fetchFirstGeneration();
 }
 
+async function renderFirstGenerationShiny(){
+    removerConteudo();
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerText = "";
+    await fetchFirstGenerationShiny();
+}
+
 async function renderSecondGeneration(){
     removerConteudo();
     let allPokemonContainer = document.querySelector('#poke-container')
@@ -31,11 +47,25 @@ async function renderSecondGeneration(){
     await fetchSecondGeneration();
 }
 
+async function renderSecondGenerationShiny(){
+    removerConteudo();
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerHTML="";
+    await fetchSecondGenerationShiny();
+}
+
 async function renderThirdGeneration(){
     removerConteudo();
     let allPokemonContainer = document.querySelector('#poke-container')
     allPokemonContainer.innerHTML="";
     await fetchThirdGeneration();
+}
+
+async function renderThirdGenerationShiny(){
+    removerConteudo();
+    let allPokemonContainer = document.querySelector('#poke-container')
+    allPokemonContainer.innerHTML="";
+    await fetchThirdGenerationShiny();
 }
 
 async function fetchFirstGeneration(){
@@ -48,12 +78,32 @@ async function fetchFirstGeneration(){
     })
 }
 
+async function fetchFirstGenerationShiny(){
+    await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(async response => await response.json())
+    .then(async function(allpokemon){
+        for (let pokemon of allpokemon.results){
+            await fetchPokemonDataShiny(pokemon)
+        }
+    })
+}
+
 async function fetchSecondGeneration(){
     await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=151')
     .then(async response => await response.json())
     .then(async function(allpokemon){
         for (let pokemon of allpokemon.results){
             await fetchPokemonData(pokemon)
+        }
+    })
+}
+
+async function fetchSecondGenerationShiny(){
+    await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=151')
+    .then(async response => await response.json())
+    .then(async function(allpokemon){
+        for (let pokemon of allpokemon.results){
+            await fetchPokemonDataShiny(pokemon)
         }
     })
 }
@@ -69,6 +119,16 @@ async function fetchThirdGeneration(){
     
 }
 
+async function fetchThirdGenerationShiny(){
+    await fetch('https://pokeapi.co/api/v2/pokemon?limit=135&offset=251')
+    .then(async response => await response.json())
+    .then(async function(allpokemon){
+        for (let pokemon of allpokemon.results){
+            await fetchPokemonDataShiny(pokemon)
+        }
+    })
+}
+
 async function fetchPokemonData(pokemon){
     let url = pokemon.url // <--- this is saving the pokemon url to a variable to use in the fetch. 
                                 //Example: https://pokeapi.co/api/v2/pokemon/1/"
@@ -76,6 +136,16 @@ async function fetchPokemonData(pokemon){
     .then(response => response.json())
     .then(function(pokeData){
         renderPokemon(pokeData)
+    })
+}
+
+async function fetchPokemonDataShiny(pokemon){
+    let url = pokemon.url // <--- this is saving the pokemon url to a variable to use in the fetch. 
+                                //Example: https://pokeapi.co/api/v2/pokemon/1/"
+    await fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData){
+        renderPokemonShiny(pokeData)
     })
 }
 
@@ -93,6 +163,29 @@ function renderPokemon(pokeData){
 
     let pokeNumber = document.createElement('p')
     pokeNumber.innerText = `#${pokeData.id}`
+   
+    let pokeTypes = document.createElement('ul') //ul list will hold the pokemon types
+  
+
+    createTypes(pokeData.types, pokeTypes) // helper function to go through the types array and create li tags for each one
+
+    pokeContainer.append(pokeName, pokeNumber, pokeTypes);   //appending all details to the pokeContainer div
+    allPokemonContainer.appendChild(pokeContainer);       //appending that pokeContainer div to the main div which will                                                             hold all the pokemon cards
+}
+
+function renderPokemonShiny(pokeData){
+    let allPokemonContainer = document.getElementById('poke-container');
+    let pokeContainer = document.createElement("div") //div will be used to hold the data/details for indiviual pokemon.{}
+    pokeContainer.classList.add('ui', 'card');
+
+    createPokeImageShiny(pokeData.id, pokeContainer);
+
+    let pokeName = document.createElement('h4') 
+    pokeName.innerText = pokeData.name
+    pokeName.innerText = pokeName.innerText.charAt(0).toUpperCase() + pokeName.innerText.slice(1) + " Shiny";
+
+    let pokeNumber = document.createElement('p')
+    pokeNumber.innerHTML = `#<strong>${pokeData.id}</strong>`
    
     let pokeTypes = document.createElement('ul') //ul list will hold the pokemon types
   
@@ -121,6 +214,18 @@ function createPokeImage(pokeID, containerDiv){
     pokeImgContainer.append(pokeImage);
     containerDiv.append(pokeImgContainer);
 }
+
+function createPokeImageShiny(pokeID, containerDiv){
+    let pokeImgContainer = document.createElement('div')
+    pokeImgContainer.classList.add('image')
+
+    let pokeImage = document.createElement('img')
+    pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeID}.png`
+
+    pokeImgContainer.append(pokeImage);
+    containerDiv.append(pokeImgContainer);
+}
+
 
 function removerConteudo(){
     document.getElementById("poke-container").innerHTML = "";
